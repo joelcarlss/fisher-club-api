4c33682280d9fd352c05655503f3f30071ae27b2
joel <zorcey@gmail.com>
2019-02-26 14:42:13 +0100

    Fixing port

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ require('./routes/webhook')(server)
 server.get('/hello/:name', respond)
 server.head('/hello/:name', respond)
 
-server.listen(8080, function () {
+server.listen(process.env.PORT || 8080, function () {
   console.log('%s listening at %s', server.name, server.url)
 })
